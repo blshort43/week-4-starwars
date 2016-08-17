d6b6e0f09909621e541af073886de0454857fca7
Brandon <blshort43@gmail.com>
2016-08-16 19:07:07 -0500

    actually got enemies to stay in enemy div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,50 @@
 
-
-
-
-$("#img1" && ".imgdiv").click(function(){
-	$("#img2, #img3, #img4").appendTo("#fightContainer").removeClass("imgdiv").addClass("enemydiv");
-});
-
-$("#img2" && "imgdiv").click(function() {
-	$("#img1, #img3, #img4").appendTo("#fightContainer").addClass("enemyBackground");
-});
-
-$("#img" && "imgdiv").click(function() {
-	$("#img1, #img2, #img4").appendTo("#fightContainer").addClass("enemyBackground");
-});
-
-$("#img" && "imgdiv").click(function() {
-	$("#img1, #img2, #img3").appendTo("#fightContainer").addClass("enemyBackground");
+$(".imgdiv").click(function() {
+	console.log(this);
+    console.log('I was clicked');
 
 });
 
+
+
+
+
+
+
+$("#img1").click(function() {
+	$(this).removeClass("character");
+	$(".character").appendTo("#fightContainer").addClass("enemy");
+});
+
+$("#img2").click(function(){
+	$(this).removeClass("character");
+	$(".character").appendTo("#fightContainer").addClass("enemy");
+});
+
+$("#img3").click(function(){
+	$(this).removeClass("character");
+	$(".character").appendTo("#fightContainer").addClass("enemy");
+});
+
+$("#img4").click(function(){
+	$(this).removeClass("character");
+	$(".character").appendTo("#fightContainer").addClass("enemy");
+});
+
+
+
+
+// $(".character").click(function() {
+// 	$("#img2").removeClass("character");
+// 	$("#img1, #img3, #img4").appendTo("#fightContainer").removeClass("character").addClass("enemy");
+
+// });
+
+// $(".character").click(function() {
+// 	$("#img1, #img2, #img4").appendTo("#fightContainer").removeClass("character").addClass("enemy");
+// });
+
+// $(".character").click(function() {
+// 	$("#img1, #img2, #img3").appendTo("#fightContainer").removeClass("character").addClass("enemy");
+
+// });
