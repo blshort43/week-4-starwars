129b8c14bbf696ab423865894610785c6c948dbc
Brandon <blshort43@gmail.com>
2016-08-17 16:30:17 -0500

    styling changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,70 @@
-
 $(".imgdiv").click(function() {
-	// console.log(this);
+    // console.log(this);
     console.log('I was clicked');
-
 });
 
-
-
-
-
-
-
-$("#img1.character").click(function() {
-	$(this).removeClass("character");
-	$(".character").appendTo("#fightContainer").removeClass("character").addClass("enemy");
-	$("#img2").addClass("enemy2");
-	$("#img3").addClass("enemy3");
-	$("#img4").addClass("enemy4");
+$("#img1").click(function() {
+    $(this).removeClass("character");
+    $("br").remove();
+    $("#charh2").insertBefore("#charContainer");
+    $(".character").appendTo("#enemies").removeClass("character").addClass("enemy");
+    $("#img2").addClass("enemy2");
+    $("#img3").addClass("enemy3");
+    $("#img4").addClass("enemy4");
 });
 
-$("#img2.character").click(function(){
-	$(this).removeClass("character");
-	$(".character").appendTo("#fightContainer").removeClass("character").addClass("enemy");
-	$("#img1").addClass("enemy1");
-	$("#img3").addClass("enemy3");
-	$("#img4").addClass("enemy4");
+$("#img2").click(function() {
+    $(this).removeClass("character");
+    $("br").remove();
+    $("#charh2").insertBefore("#charContainer");
+    $(".character").appendTo("#enemies").removeClass("character").addClass("enemy");
+    $("#img1").addClass("enemy1");
+    $("#img3").addClass("enemy3");
+    $("#img4").addClass("enemy4");
 });
 
-$("#img3.character").click(function(){
-	$(this).removeClass("character");
-	$(".character").appendTo("#fightContainer").removeClass("character").addClass("enemy");
-	$("#img1").addClass("enemy1");
-	$("#img2").addClass("enemy2");
-	$("#img4").addClass("enemy4");
+$("#img3").click(function() {
+    $(this).removeClass("character");
+    $("br").remove();
+    $("#charh2").insertBefore("#charContainer");
+    $(".character").appendTo("#enemies").removeClass("character").addClass("enemy");
+    $("#img1").addClass("enemy1");
+    $("#img2").addClass("enemy2");
+    $("#img4").addClass("enemy4");
 });
 
-$("#img4.character").click(function(){
-	$(this).removeClass("character");
-	$(".character").appendTo("#fightContainer").removeClass("character").addClass("enemy");
-	$("#img1").addClass("enemy1");
-	$("#img2").addClass("enemy2");
-	$("#img3").addClass("enemy3");
+$("#img4").click(function() {
+    $(this).removeClass("character");
+    $("br").remove();
+    $("#charh2").insertBefore("#charContainer");
+    $(".character").appendTo("#enemies").removeClass("character").addClass("enemy");
+    $("#img1").addClass("enemy1");
+    $("#img2").addClass("enemy2");
+    $("#img3").addClass("enemy3");
 });
 
-$("#img1").click(function(){
- 	if($("#defender").is(":empty") && $(this).hasClass("enemy1")){
- 		$(this).appendTo("#defender").addClass("defender");
- 	}
+$("#img1").click(function() {
+    if ($("#defender").is(":empty") && $(this).hasClass("enemy1")) {
+        $(this).appendTo("#defender").removeClass("enemy").addClass("defender");
+    }
 });
 
-$("#img2").click(function(){
- 	if($("#defender").is(":empty") && $(this).hasClass("enemy2")){
- 		$(this).appendTo("#defender").addClass("defender");
- 	}
+$("#img2").click(function() {
+    if ($("#defender").is(":empty") && $(this).hasClass("enemy2")) {
+        $(this).appendTo("#defender").removeClass("enemy").addClass("defender");
+    }
 });
 
-$("#img3").click(function(){
- 	if($("#defender").is(":empty") && $(this).hasClass("enemy3")){
- 		$(this).appendTo("#defender").addClass("defender");
- 	}
+$("#img3").click(function() {
+    if ($("#defender").is(":empty") && $(this).hasClass("enemy3")) {
+        $(this).appendTo("#defender").removeClass("enemy").addClass("defender");
+    }
 });
 
-$("#img4").click(function(){
- 	if($("#defender").is(":empty") && $(this).hasClass("enemy4")){
- 		$(this).appendTo("#defender").addClass("defender");
- 	}
+$("#img4").click(function() {
+    if ($("#defender").is(":empty") && $(this).hasClass("enemy4")) {
+        $(this).appendTo("#defender").removeClass("enemy").addClass("defender");
+    }
 });
 
 // $(".enemy3").click(function(){
