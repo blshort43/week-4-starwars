d5f88c4ad0a0edbfbe4a763336bcf184b5ff212e
Brandon <blshort43@gmail.com>
2016-08-18 14:51:47 -0500

    created character obtjects, gave characters health

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,42 @@ $(".imgdiv").click(function() {
     console.log('I was clicked');
 });
 
+// create objects for each character
+var keanu = {
+	"name": "Conspiracy Keanu",
+	"healthPoints": 120,
+	"attackPower": 5,
+	"counterAttackPower": 10
+};
+
+var grumpy = {
+	"name": "Grumpy Cat",
+	"healthPoints": 100,
+	"attackPower": 10,
+	"counterAttackPower": 15
+};
+
+var scumbag = {
+	"name": "Scumbag Steve",
+	"healthPoints": 150,
+	"attackPower": 15,
+	"counterAttackPower": 20
+};
+
+var doge = {
+	"name": "Doge",
+	"healthPoints": 180,
+	"attackPower": 25,
+	"counterAttackPower": 30
+};
+
+// display character's health
+$(".keanuP").append(keanu.healthPoints); 
+$(".grumpyP").append(grumpy.healthPoints);
+$(".scumbagP").append(scumbag.healthPoints);
+$(".dogeP").append(doge.healthPoints);
+
+// make character choices and move to different divs on click
 $(".imgdiv").click(function() {
     $(this).removeClass("character");
     $("br").remove();
