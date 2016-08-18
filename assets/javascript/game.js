e04de56bc6d561c0a7904ff7624e94b792cd3a8d
Brandon <blshort43@gmail.com>
2016-08-18 11:39:48 -0500

    shortened code to 1 paragraph, made it reusable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,101 +3,13 @@ $(".imgdiv").click(function() {
     console.log('I was clicked');
 });
 
-$("#img1").click(function() {
+$(".imgdiv").click(function() {
     $(this).removeClass("character");
     $("br").remove();
     $("#charh2").insertBefore("#charContainer");
     $(".character").appendTo("#enemies").removeClass("character").addClass("enemy");
-    $("#img2").addClass("enemy2");
-    $("#img3").addClass("enemy3");
-    $("#img4").addClass("enemy4");
-});
-
-$("#img2").click(function() {
-    $(this).removeClass("character");
-    $("br").remove();
-    $("#charh2").insertBefore("#charContainer");
-    $(".character").appendTo("#enemies").removeClass("character").addClass("enemy");
-    $("#img1").addClass("enemy1");
-    $("#img3").addClass("enemy3");
-    $("#img4").addClass("enemy4");
-});
-
-$("#img3").click(function() {
-    $(this).removeClass("character");
-    $("br").remove();
-    $("#charh2").insertBefore("#charContainer");
-    $(".character").appendTo("#enemies").removeClass("character").addClass("enemy");
-    $("#img1").addClass("enemy1");
-    $("#img2").addClass("enemy2");
-    $("#img4").addClass("enemy4");
-});
-
-$("#img4").click(function() {
-    $(this).removeClass("character");
-    $("br").remove();
-    $("#charh2").insertBefore("#charContainer");
-    $(".character").appendTo("#enemies").removeClass("character").addClass("enemy");
-    $("#img1").addClass("enemy1");
-    $("#img2").addClass("enemy2");
-    $("#img3").addClass("enemy3");
-});
-
-$("#img1").click(function() {
-    if ($("#defenderDiv").is(":empty") && $(this).hasClass("enemy1")) {
-        $(this).appendTo("#defenderDiv").removeClass("enemy").addClass("defender");
+    if ($("#defenderDiv").is(":empty") && $(this).hasClass("enemy")) {
+    	$(this).appendTo("#defenderDiv").removeClass("enemy").addClass("defender");
     }
 });
 
-$("#img2").click(function() {
-    if ($("#defenderDiv").is(":empty") && $(this).hasClass("enemy2")) {
-        $(this).appendTo("#defenderDiv").removeClass("enemy").addClass("defender");
-    }
-});
-
-$("#img3").click(function() {
-    if ($("#defenderDiv").is(":empty") && $(this).hasClass("enemy3")) {
-        $(this).appendTo("#defenderDiv").removeClass("enemy").addClass("defender");
-    }
-});
-
-$("#img4").click(function() {
-    if ($("#defenderDiv").is(":empty") && $(this).hasClass("enemy4")) {
-        $(this).appendTo("#defenderDiv").removeClass("enemy").addClass("defender");
-    }
-});
-
-// $(".enemy3").click(function(){
-//  	if($("#defender").is(':empty')){
-//  		$(".enemy3").appendTo("#defender").addClass("defender");
-//  	}
-// });
-
-// $(".enemy4").click(function(){
-//  	if($("#defender").is(':empty')){
-//  		$(".enemy4").appendTo("#defender").addClass("defender");
-//  	}
-// });
-
-// if ($("#defender") === " ")function(){
-// 	$(".enemy").click(function(){
-// 		$(this).removeClass("enemy");
-// 		$(".enemy").appendTo("#defender").addClass("defender");
-// 	})
-
-
-
-// $(".character").click(function() {
-// 	$("#img2").removeClass("character");
-// 	$("#img1, #img3, #img4").appendTo("#fightContainer").removeClass("character").addClass("enemy");
-
-// });
-
-// $(".character").click(function() {
-// 	$("#img1, #img2, #img4").appendTo("#fightContainer").removeClass("character").addClass("enemy");
-// });
-
-// $(".character").click(function() {
-// 	$("#img1, #img2, #img3").appendTo("#fightContainer").removeClass("character").addClass("enemy");
-
-// });
