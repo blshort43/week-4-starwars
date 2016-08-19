be3891f33a27b8a255d3f7ddd9bb7a9a094fcf9a
Brandon <blshort43@gmail.com>
2016-08-19 17:06:12 -0500

    got fight button to work partially

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,49 +3,59 @@ $(".imgdiv").click(function() {
     console.log('I was clicked');
 });
 
-// create objects for each character
-var keanu = {
-	"name": "Conspiracy Keanu",
-	"healthPoints": 120,
-	"attackPower": 5,
-	"counterAttackPower": 10
-};
+// create attributes for each character
+var keanu = $("#img1");
+keanu.attr("healthPoints", 120);
+keanu.attr("attackPower", 5);
+keanu.attr("counterAttackPower", 10);
 
-var grumpy = {
-	"name": "Grumpy Cat",
-	"healthPoints": 100,
-	"attackPower": 10,
-	"counterAttackPower": 15
-};
+var grumpy = $("#img2");
+grumpy.attr("healthPoints", 100);
+grumpy.attr("attackPower", 10);
+grumpy.attr("counterAttackPower", 15);
 
-var scumbag = {
-	"name": "Scumbag Steve",
-	"healthPoints": 150,
-	"attackPower": 15,
-	"counterAttackPower": 20
-};
+var scumbag = $("#img3")
+scumbag.attr("healthPoints", 150);
+scumbag.attr("attackPower", 15);
+scumbag.attr("counterAttackPower", 20);
 
-var doge = {
-	"name": "Doge",
-	"healthPoints": 180,
-	"attackPower": 25,
-	"counterAttackPower": 30
-};
+var doge = $("#img4")
+doge.attr("healthPoints", 180);
+doge.attr("attackPower", 20);
+doge.attr("counterAttackPower", 25);
+
 
 // display character's health
-$(".keanuP").append(keanu.healthPoints); 
-$(".grumpyP").append(grumpy.healthPoints);
-$(".scumbagP").append(scumbag.healthPoints);
-$(".dogeP").append(doge.healthPoints);
+// $("#img1").append(keanu.attr('healthPoints'));
+// $("#img2").append(grumpy.attr('healthPoints'));
+// $("#img3").append(scumbag.attr('healthPoints'));
+// $("#img4").append(doge.attr('healthPoints'));
 
 // make character choices and move to different divs on click
 $(".imgdiv").click(function() {
-    $(this).removeClass("character");
+    $(this).removeClass("character").addClass("champ");
+    $(".title").remove();
     $("br").remove();
     $("#charh2").insertBefore("#charContainer");
     $(".character").appendTo("#enemies").removeClass("character").addClass("enemy");
     if ($("#defenderDiv").is(":empty") && $(this).hasClass("enemy")) {
-    	$(this).appendTo("#defenderDiv").removeClass("enemy").addClass("defender");
+    	$(this).appendTo("#defenderDiv").removeClass("champ").addClass("defender");
     }
 });
+
+
+$(".btn").click(function(){
+//decrease defender's health when champ attacks
+var defenderHealthLoss = parseInt($(".defender").attr('healthPoints')) - parseInt($(".champ").attr('attackPower'));
+$(".defender").attr("healthPoints", defenderHealthLoss);
+var updateNumber = (defenderHealthLoss);
+$(".defender p").html(updateNumber);
+
+//decrease champ's health when defender counter attacks
+var counterAttack = parseInt($(".champ").attr('healthPoints')) - parseInt($(".defender").attr('counterAttackPower'));
+$(".champ").attr("healthPoints", counterAttack);
+var updateNumber = (counterAttack);
+$(".champ p").html(updateNumber);
+
+})
 
